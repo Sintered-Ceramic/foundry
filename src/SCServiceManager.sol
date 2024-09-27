98084fed3465bb12330361b1cc4e05a91cca3908
taylor hulsmans <thulsmans.1133@gmail.com>
2024-09-27 09:13:34 -0700

    imp

---
+++ src/SCServiceManager.sol
@@ -0,0 +1,8 @@
+pragma solidity 0.8.26;
+
+import {ECDSAServiceManagerBase} from "@eigenlayer-middleware/src/unaudited/ECDSAServiceManagerBase.sol";
+import {ECDSAStakeRegistry} from "@eigenlayer-middleware/src/unaudited/ECDSAStakeRegistry.sol";
+import {IServiceManager} from "@eigenlayer-middleware/src/interfaces/IServiceManager.sol";
+import {ECDSAUpgradeable} from "@openzeppelin-upgrades/contracts/utils/cryptography/ECDSAUpgradeable.sol";
+import {IHelloWorldServiceManager} from "./IHelloWorldServiceManager.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
